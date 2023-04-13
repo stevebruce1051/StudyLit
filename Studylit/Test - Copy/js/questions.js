830b32d9e16bd315f66a6d939127cc3cb9142a86
stevebruce1051 <70656102+stevebruce1051@users.noreply.github.com>
2023-04-13 03:48:21 +0000

    Add files via upload

---
+++ Studylit/Test - Copy/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "According to the story, what role did birds play during wars?",
+    answer: "Birds sent messages to the soldiers",
+    options: [
+      "Birds played with children",
+      "Birds sang to soldiers",
+      "Birds sent messages to the soldiers",
+      "Birds ate a lot of seeds"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What photograph was on the calendar Debbie turned over",
+    answer:   "A countryside scene",
+    options: [
+      "A picture of sparrows",
+      "A countryside scene",
+      "A garden scene",
+      "A colourful array of plants"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who first found Pepe",
+    answer: "Debbie",
+    options: [
+      "Debbie",
+      "Mum",
+      "Sandy",
+      "Mrs. Atkins"
+    ]
+  },
+    {
+    numb: 4,
+    question: "'Pepe' as used in the story, refers to ",
+    answer: "A bird",
+    options: [
+      "Sandy's younger sister",
+      "Debbie's classmate",
+      "A bird",
+      "A favorite book"
+    ]
+  },
+    {
+    numb: 5,
+    question: "________ are awfully hard to rear",
+    answer: "Birds",
+    options: [
+      "Dogs",
+      "Worms",
+      "Cats",
+      "Birds"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
